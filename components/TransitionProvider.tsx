04f57e7b83ae1439d6d3db12042a80c5d2b5b74d
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-30 17:09:59 +0100

    Update TransitionProvider.tsx

--- components/TransitionProvider.tsx
+++ components/TransitionProvider.tsx
@@ -11,7 +11,7 @@ const TransitionProvider = ({ children }: {children:React.ReactNode;}) => {
     <AnimatePresence mode="wait">
       <div key={pathname}>
         <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40" animate={{height:"0vh"}} exit={{height:"140vh"}} transition={{duration:0.5, ease:"easeOut"}}/>
-        <motion.div className="fixed m-auto top-0 bottom-0 left-0 right-0 text-8xl max-md:text-4xl capitalize z-50 text-white w-fit h-fit" initial={{opacity: 1}} animate={{opacity:0}} exit={{opacity:0}} transition={{duration:0.8, ease:"easeOut"}}>{pathname.substring(1)}</motion.div>
+        <motion.div className="fixed m-auto top-0 bottom-0 left-0 right-0 text-8xl max-md:text-2xl capitalize z-50 text-white w-fit h-fit" initial={{opacity: 1}} animate={{opacity:0}} exit={{opacity:0}} transition={{duration:0.8, ease:"easeOut"}}>{pathname.substring(1)}</motion.div>
         <motion.div className="h-screen w-screen fixed bottom-0 bg-black rounded-t-[100px] z-30" initial={{height:"140vh"}} animate={{height:"0vh", transition: {delay:0.5}}} transition={{duration:0.5, ease:"easeOut"}}/>
       </div>
       <Navbar />
