22bcd013b03fb91e9a974e554bcb172f3b059eb0
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-31 14:55:07 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { Github, Linkedin, ExternalLink, AlignJustify } from "lucide-react"
 import { navigation } from "../constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
